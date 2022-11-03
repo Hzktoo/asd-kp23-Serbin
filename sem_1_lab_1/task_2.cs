68905e08a3c87e235b44f5a048bb1e5a88cbcedb
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-03 21:04:27 +0200

    task_2_second_commit

--- sem_1_lab_1/task_2.cs
+++ sem_1_lab_1/task_2.cs
@@ -5,6 +5,18 @@ namespace assingment1
     {
         static public void Main()
         {
+            /* test scenarios
+       c = 14
+       result: False century
+       c = 22
+       result: count = 166
+       c = 45
+       result: count = 165
+       c = 50
+       result: count = 7
+       c = 65
+       result: False century
+         */
             int c, d;
             int count;
             Console.WriteLine(count);
