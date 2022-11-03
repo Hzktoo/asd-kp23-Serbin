06e8b30d127c4a3b580895c97ac6f5750342ac2a
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-03 16:15:48 +0200

    task_1_second_commit

--- sem_1_lab_1/task_1.cs
+++ sem_1_lab_1/task_1.cs
@@ -1,10 +1,26 @@
-﻿using System;
+using System;
 namespace assingment1
 {
     class task3
     {
         static public void Main()
         {
+            /* 
+             test scenarios
+            x = 5, y = 6, z = 7
+            a = 13,079181246047625, b = 0,016921997461237535
+            x = 0, y = 5, z = 1
+            The result is uncertain
+            x = 1, y = 16, z = 0
+            The result is uncertain
+            x = 0, y = 54, z = 139
+            The result is uncertain
+            x = 81, y = 42, z = 6
+            a = 84,68491150050218, b = 0,0007125515553557031
+            x = 125, y = 125, z = -1500
+            The result is uncertain
+             */
+            
             double x, z, y;
             double a, b;
             Console.WriteLine("a =" + a);
