379799d476da2c5daf7acf1f0dbb1b2665aab0f1
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-03 16:32:20 +0200

    task_2_first_commit

---
+++ sem_1_lab_1/task_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace assingment1
+{
+    class task13
+    {
+        static public void Main()
+        {
+            int c, d;
+            int count;
+            Console.WriteLine(count);
+        }
+    }
+}
