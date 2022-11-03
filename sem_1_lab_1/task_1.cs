6c6a4697dda35a0ca585d363fdd10ccd63eb7203
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-03 15:57:28 +0200

    task_1_first_commit

---
+++ sem_1_lab_1/task_1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace assingment1
+{
+    class task3
+    {
+        static public void Main()
+        {
+            double x, z, y;
+            double a, b;
+            Console.WriteLine("a =" + a);
+            Console.WriteLine("b =" + b;
+        }
+    }
+}
