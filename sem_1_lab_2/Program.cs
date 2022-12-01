93097b15ef9943426f9a26388532c4b9f835140b
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-12-01 22:07:13 +0200

    code_first_commit

---
+++ sem_1_lab_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace assingment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, M;
+            int max1;
+            double halfsum;
+            Console.WriteLine(max1);
+            Console.WriteLine(halfsum);  
+        }
+    }
+}
