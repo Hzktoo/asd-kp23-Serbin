9ab9d059ff73c5298655e90dbacbe3974cf3cea0
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-04 14:14:22 +0200

    third_commit_of_code

--- sem_2_lab_1/Program.cs
+++ sem_2_lab_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 class Program
 {
-    static bool testElemWhichWillSort()
+    static bool TestElemWhichWillSort()
     {
         int n = 5;
         int[] testgrid1 = { 9, 12, 3, 5, 4, 7, 11, 18, 1, 8 };
@@ -19,7 +19,7 @@ class Program
             return true;
         }
     }
-    static bool testMySort()
+    static bool TestMySort()
     {
         int[] testgrid1 = { 9, 12, 3, 5, 4, 7, 11, 18, 1, 8 };
         int[] result1 = { 18, 12, 11, 9, 8, 7, 5, 4, 3, 1 };
@@ -38,11 +38,24 @@ class Program
     }
     static void Main(string[] args)
     {
-        int N = 0;
+        Console.WriteLine("Enter the size of array:");
+        int N = int.Parse(Console.ReadLine());
+        if (N <= 0)
+        {
+            Console.WriteLine("Please, enter a positive size");
+            return;
+        }
         int n = N / 2;
         int[] arr = GenerateRandomArray(N);
+        Console.WriteLine("Generated array:");
         PrintArray(arr);
+        Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Colored array:");
         PrintColoredArray(arr);
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Sorted array:");
         int[] arr2 = ElemWhichWillSort(arr, n);
         PrintSortedArray(arr2);
         PrintUnsortedArray(arr);
@@ -50,37 +63,116 @@ class Program
     static int[] GenerateRandomArray(int N)
     {
         int[] arr = new int[N];
+        Random random = new Random();
+        for (int i = 0; i < N; i++)
+        {
+            arr[i] = random.Next(0, N);
+        }
         return arr;
     }
     static void PrintArray(int[] arr)
     {
-        Console.WriteLine(arr);
+        int N = arr.Length;
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
     }
     static void PrintColoredArray(int[] arr)
     {
-        Console.WriteLine(arr);
+        int N = arr.Length;
+        int n = arr.Length / 2;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] >= n)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.Write(arr[i] + " ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
     }
     static int[] ElemWhichWillSort(int[] arr, int n)
     {
-        int[] arr2 = arr;
+        int count = 0;
+        int N = arr.Length;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] < n)
+            {
+                count++;
+            }
+        }
+        int[] arr2 = new int[count];
+        int index = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] < n)
+            {
+                arr2[index] = arr[i];
+                index++;
+            }
+        }
         return arr2;
     }
     static void PrintSortedArray(int[] arr)
     {
         arr = MySort(arr);
-        Console.WriteLine(arr);
+        int N = arr.Length;
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
     }
     static void PrintUnsortedArray(int[] arr)
     {
-        Console.WriteLine(arr);
+        int N = arr.Length;
+        int n = N / 2;
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            if (arr[i] >= n)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(arr[i] + " ");
+            }
+            Console.ResetColor();
+        }
+        Console.WriteLine();
     }
     static int[] MySort(int[] arr)
     {
+        int N = arr.Length;
+        bool wasExchange = false;
+        for (int i = 0; i < N / 2; i++)
+        {
+            for (int j = i; j < N - i - 1; j++)
+            {
+                if (arr[j] < arr[j + 1])
+                {
+                    Exchanelem(ref arr[j], ref arr[j + 1]);
+                    wasExchange = true;
+                }
+            }
+            for (int j = N - 2 - i; j > i; j--)
+            {
+                if (arr[j - 1] < arr[j])
+                {
+                    Exchanelem(ref arr[j - 1], ref arr[j]);
+                    wasExchange = true;
+                }
+            }
+            if (!wasExchange)
+            {
+                break;
+            }
+        }
         return arr;
     }
     static void Exchanelem(ref int arr1, ref int arr2)
     {
-        var temp = arr1;
+        int temp = arr1;
         arr1 = arr2;
         arr2 = temp;
     }
